8ffa2138df9755d3dbb46e11c77afc3859f0048d
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2024-10-27 20:02:36 -0300

    feat: página de inicio y componentes * Se agrega página de inicio * Se agregan componentes 'Input' y 'Label' * Se mejora componente 'Button'

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,16 +1,24 @@
 import {createBrowserRouter} from "react-router-dom";
 import Layout from "@/layout/Layout.tsx";
 import Home from "@/pages/home/home.tsx";
+import Login from "@/pages/auth/login/login.tsx";
 
 const routes = [
   {
     path: "/",
     element: <Layout/>,
     children: [
+      /* Home */
       {
         index: true,
         element: <Home />,
       },
+
+      /* Auth */
+      {
+        path: '/login',
+        element: <Login/>
+      }
     ],
   },
 ];
